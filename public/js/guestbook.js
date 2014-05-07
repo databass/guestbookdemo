60603389d01795774b6854fba2ce3aafc9b85262
David Byman <dbvt@databass.com>
2014-05-07 11:24:16 -0400

    Create guestbook.js
    GB Modules

---
+++ public/js/guestbook.js
@@ -0,0 +1,150 @@
+/**
+ * guestbook.js
+ */
+ 
+
+var GB = (function () {
+
+    var selectedOrgName = null;
+    var that = this;
+
+    return {
+
+
+        "signGuestbook": function () {
+
+            that = this;
+
+            $('#myPostModal').modal({
+                show: true
+            });
+            //		    $('#myPostModal .modal-body').html(_.template($('#ErrorTemplate').html(), data));
+
+            return false;
+        },
+
+
+        "getMessages": function () {
+
+            var that = this;
+
+
+            $.getJSON("/getmessages", function (data) {
+
+                data.messages = data;
+
+                $("#GuestbookContainer").html(_.template($('#GuestbookTemplate').html(), data.messages));
+
+                $("#GuestbookContainer").show();
+
+                $('#GuestbookTable').dataTable({
+                    "aaSorting": [
+                        [3, "desc"]
+                    ],
+                    "bPaginate": false,
+                    "bInfo": false,
+                    "bFilter": false,
+                    "bAutoWidth": true,
+                    "oLanguage": {
+                        "sSearch": "Filter: "
+                    }
+                });
+
+                $('.deleteEntry').on('click', function (event) {
+
+
+                    that.deleteMessageConfirm($(this).data("id"));
+
+                });
+
+
+            });
+
+        },
+
+        "deleteMessageConfirm": function (messageId) {
+
+
+
+            $('#myDeleteConfirmModal').modal({
+                show: true
+            });
+
+            $('#DeleteMessage').data('messageId', messageId);
+
+
+        },
+
+
+        "deleteMessage": function (messageId) {
+
+            $('#myDeleteConfirmModal').modal({
+                show: false
+            });
+
+
+            $.getJSON("/deletemessage/" + messageId, function (data) {
+
+                $('#myDeleteModal').modal({
+                    show: true
+                });
+
+                setTimeout(function () {
+                    $('#myDeleteModal').modal('hide');
+                    that.getMessages();
+                }, 2000);
+
+
+
+            });
+        },
+
+        "postMessage": function (guestName, messageText, favoriteKeyword) {
+
+            $.getJSON("/postmessage/" + messageText + "/" + guestName + "/" + favoriteKeyword, function (data) {
+
+                $('#myPostModal').modal({
+                    show: false
+                });
+
+                $('#myThanksModal').modal({
+                    show: true
+                });
+                $("#myThanksModal .modal-body").html("Thanks for signing the guestbook, <b>" + guestName + "</b>");
+                setTimeout(function () {
+                    $('#myThanksModal').modal('hide');
+                    that.getMessages();
+                }, 2000);
+            });
+
+
+
+            that.getMessages();
+
+        },
+
+
+        "init": function () {
+
+            var that = this;
+
+            $("#SignGuestbook").on('click', function (event) {
+                that.signGuestbook();
+            });
+            $("#PostMessage").on('click', function (event) {
+                that.postMessage($("#guestName").val(), $("#messageText").val(), $("#favoriteKeyword").val());
+            });
+
+            $("#DeleteMessage").on('click', function (event) {
+
+                that.deleteMessage($(this).data('messageId'));
+            });
+            that.getMessages();
+        }
+
+
+
+
+    };
+
+})();
